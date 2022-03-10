c1b5450a47ee088b6a1de205f9230538a8acc4a2
Nicolas <nicolaslibert.pro@gmail.com>
2022-03-10 14:40:50 +0100

    Amélioration du HANGMAN

--- asset/script.js
+++ asset/script.js
@@ -1,5 +1,6 @@
 const word = document.getElementById('word');
 const wrongLetters = document.getElementById('lettersUsed');
+const notif = document.getElementById('notifContainer');
 
 //On récupére les différents éléments composant la victime
 const victimParts = document.querySelectorAll('.victim');
@@ -14,8 +15,26 @@ let wordSelected = wordsToFind[Math.floor(Math.random() * wordsToFind.length)];
 const wrongLettersUsed = [];
 const correctLetters = [];
 
+
+//TO DO LIST :
+// Faire apparaitre les mots
+// Faire le game over et le sceen de victoire
+
+function notifBox()
+{
+        // Affiche le msg si la lettre a déjà été tapé
+        notif.style.display = "block";
+
+        // Cache le msg après quelques secondes
+        setTimeout(function() {
+            notif.style.display = "none";
+        }, 1000);
+
+}
+
 function victim()
 {
+    
     wrongLetters.innerHTML = wrongLettersUsed.map(letter => letter);
 
     //Affiche les parties de la victime en fonction du nombre d'erreurs
@@ -23,13 +42,59 @@ function victim()
         const errors = wrongLettersUsed.length;
 
         if(index < errors) {
-            part.style.display = 'block'
+            part.style.display = 'block';
         }
         else{
             part.style.display = 'none';
         }
     });
+
+    //Condition de défaite
+    setTimeout(function() {
+        if(wrongLettersUsed.length === victimParts.length)
+        {
+            alert('Vous avez perdu ! Dommage pour lui...');
+            newGame();
+        }
+
+    });
 }
+
+function wordToFind()
+{
+
+    word.innerHTML = `
+    ${wordSelected
+    .split('')
+    .map(
+        letter =>`
+        <span class="letter">
+        ${correctLetters.includes(letter) ? letter : ''}
+        </span>
+        `
+    )
+    .join('')}
+    `;
+
+    // On enlève/remplace les espaces dans le résultat
+    const checkWin = word.innerText.replace(/ /g, '');
+
+    // Condition de victoire
+    setTimeout(function() {
+        if(checkWin === wordSelected)        
+        {
+            alert('Vous l\'avez sauvé ! Félicitations !');
+            newGame();
+        }
+    });
+   
+    // for(let i = 0; i < wordSelected.length; i++)
+    // {
+    //     const html = `<p class="word">_</p>`;
+    // }
+}
+
+
 
 window.addEventListener('keydown', e =>{
     if(e.keyCode >= 65 && e.keyCode <=90){
@@ -41,10 +106,11 @@ window.addEventListener('keydown', e =>{
             if(!correctLetters.includes(letter))
             {
                 correctLetters.push(letter);
-                console.log('yes');
+                wordToFind();
             }
             else
             {
+                notifBox();
                 console.log('Déjà tapée !')
             }
         }
@@ -55,12 +121,26 @@ window.addEventListener('keydown', e =>{
             {
                 wrongLettersUsed.push(letter);
                 victim();
-                console.log(wrongLettersUsed);
             }
             else
             {
+                notifBox();
                 console.log('Déjà tapée !');
             }
         }
     }
 });
+
+function newGame()
+{
+    //Supprime le contenu des tableaux
+    correctLetters.splice(0);
+    wrongLettersUsed.splice(0);
+
+    wordSelected = wordsToFind[Math.floor(Math.random() * wordsToFind.length)];
+
+    wordToFind();
+    victim();
+}
+
+wordToFind();
